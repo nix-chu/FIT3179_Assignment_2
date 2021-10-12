ea4c04af29afb0442435715a40c6b8893f64e6f5
nix-chu <nmchua8@gmail.com>
2021-10-13 02:39:24 +1100

    Create Electoral College map visualisation

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,3 +3,21 @@ let electoralCollegeMap = "electoralCollegeMap.vg.json";
 vegaEmbed("#electoralCollegeMap", electoralCollegeMap).then(function(result) {
 
 }).catch(console.error);
+
+let popularVoteMap = "popularVoteMap.vg.json";
+
+vegaEmbed("#popularVoteMap", popularVoteMap).then(function(result) {
+
+}).catch(console.error);
+
+let countyVoteMap = "countyVoteMap.vg.json";
+
+vegaEmbed("#countyVoteMap", countyVoteMap).then(function(result) {
+
+}).catch(console.error);
+
+let statePopulationMap = "statePopulationMap.vg.json";
+
+vegaEmbed("#statePopulationMap", statePopulationMap).then(function(result) {
+
+}).catch(console.error);
