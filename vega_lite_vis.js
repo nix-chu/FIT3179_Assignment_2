a6965b0b05a22a90e365eb7afe5743256413cbe3
nix-chu <nmchua8@gmail.com>
2021-10-17 02:12:04 +1100

    Create State Density Map

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,29 +1,24 @@
 let electoralCollegeMap = "vegalite_code/electoralCollegeMap.vg.json";
-
 vegaEmbed("#electoralCollegeMap", electoralCollegeMap).then(function(result) {
 
 }).catch(console.error);
 
 let popularVoteChart = "vegalite_code/popularVoteChart.vg.json";
-
 vegaEmbed("#popularVoteChart", popularVoteChart).then(function(result) {
 
 }).catch(console.error);
 
 let countyVoteMap = "vegalite_code/countyVoteMap.vg.json";
-
 vegaEmbed("#countyVoteMap", countyVoteMap).then(function(result) {
 
 }).catch(console.error);
 
-let countyDorlingCartogram = "vegalite_code/countyDorlingCartogram.vg.json";
-
-vegaEmbed("#countyDorlingCartogram", countyDorlingCartogram).then(function(result) {
+let statePopulationBar = "vegalite_code/statePopulationBar.vg.json";
+vegaEmbed("#statePopulationBar", statePopulationBar).then(function(result) {
 
 }).catch(console.error);
 
 let statePopulationMap = "vegalite_code/statePopulationMap.vg.json";
-
 vegaEmbed("#statePopulationMap", statePopulationMap).then(function(result) {
 
 }).catch(console.error);
